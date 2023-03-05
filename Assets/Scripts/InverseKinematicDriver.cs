05a77d5c2bbf539656b1b5572bfa5b35e2a2706f
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-03-05 11:46:10 -0500

    making weird eyes that follow you

--- Assets/Scripts/InverseKinematicDriver.cs
+++ Assets/Scripts/InverseKinematicDriver.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class InverseKinematicDriver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // this script runs our ik
+    [SerializeField] Transform rightHandTarget, leftHandTarget; // our hand targets
 
-    // Update is called once per frame
-    void Update()
+    // set and use animator
+    Animator animator; private void Awake() => animator = GetComponent<Animator>();
+
+    // run on animator ik
+    private void OnAnimatorIK(int layerIndex)
     {
-        
+        // set hand positions & weights
+        try
+        {
+            animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandTarget.position);
+            animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+
+            animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandTarget.position);
+            animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+
+            // set hand rotations & weights
+            animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandTarget.rotation);
+            animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+
+            animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandTarget.rotation);
+            animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+        } catch { }
     }
 }
---
+++ Assets/Scripts/ProceduralEyeAnimator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralEyeAnimator : MonoBehaviour
+{
+    // this script manages our eye plants
+    [SerializeField] Transform pivot; // our eye pivot
+
+
+    void ProcessEye()
+    {
+        // look at the player
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
